c69d7ad984f6f1df2d7bf950607cedff1211dbcc
Mariia Safronova <mary.an.safronova@yandex.ru>
2024-12-04 14:09:47 +0300

    fix: added newline at end of files

--- src/components/user-info/user-info.ts
+++ src/components/user-info/user-info.ts
@@ -15,31 +15,43 @@ export default class UserInfo extends Block {
       EmailItem: new UserInfoItem({
         name: props.userInfo.email,
         value: props.userData.email,
+        error: false,
+        errorText: '',
       }),
 
       LoginItem: new UserInfoItem({
         name: props.userInfo.login,
         value: props.userData.login,
+        error: false,
+        errorText: '',
       }),
 
       FirstNameItem: new UserInfoItem({
         name: props.userInfo.first_name,
         value: props.userData.first_name,
+        error: false,
+        errorText: '',
       }),
 
       SecondNameItem: new UserInfoItem({
         name: props.userInfo.second_name,
         value: props.userData.second_name,
+        error: false,
+        errorText: '',
       }),
 
       DisplayNameItem: new UserInfoItem({
         name: props.userInfo.display_name,
         value: props.userData.display_name,
+        error: false,
+        errorText: '',
       }),
 
       PhoneItem: new UserInfoItem({
         name: props.userInfo.phone,
         value: props.userData.phone,
+        error: false,
+        errorText: '',
       }),
     });
   }
